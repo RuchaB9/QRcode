f346f71ca206937844d6ef38a59c8d9cac953dae
RuchaB9 <ruchabhidem@gmail.com>
2023-02-03 02:02:52 +0530

    QR code scanner added

--- app/src/main/java/com/example/qrcode/MainActivity.kt
+++ app/src/main/java/com/example/qrcode/MainActivity.kt
@@ -1,56 +1,29 @@
 package com.example.qrcode
 
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.Toast
-import com.google.zxing.BarcodeFormat
-import com.google.zxing.WriterException
-import com.google.zxing.qrcode.QRCodeWriter
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var QRcode : ImageView
-    private lateinit var getData: EditText
-    private lateinit var btngetQRcode : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        QRcode=findViewById(R.id.QRcode)
-        getData=findViewById(R.id.getData)
-        btngetQRcode=findViewById(R.id.generateQRcode)
+        findViewById<Button>(R.id.scanbutton).setOnClickListener {
+            startActivity(Intent(this,scan_qr::class.java))
+        }
+        findViewById<Button>(R.id.generatebutton).setOnClickListener {
+            startActivity(Intent(this,generate_qr::class.java))
+        }
 
-        btngetQRcode.setOnClickListener{
 
-            val data= getData.text.toString().trim()
-
-            if(data.isEmpty()){
-                Toast.makeText(this, "Account not found, please register!", Toast.LENGTH_SHORT).show()
-            } else {
-                val writer= QRCodeWriter()
-                try{
-
-                    val bitMatrix= writer.encode(data, BarcodeFormat.QR_CODE, 512, 512)
-                    val width= bitMatrix.width
-                    val height=bitMatrix.height
-                    val bmp= Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
-                    for(x in 0 until width){
-                        for(y in 0 until height){
-                            bmp.setPixel(x,y, if(bitMatrix[x,y]) Color.BLACK else Color.WHITE)
-                        }
-                    }
-                    QRcode.setImageBitmap(bmp)
-                } catch(e: WriterException) {
-                    e.printStackTrace()
-                }
-            }
         }
 
     }
-}
+
---
+++ app/src/main/java/com/example/qrcode/scan_qr.kt
@@ -0,0 +1,57 @@
+package com.example.qrcode
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.appcompat.app.AppCompatActivity
+import com.journeyapps.barcodescanner.CaptureActivity
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanOptions
+
+
+class scan_qr : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.scan_qr)
+
+        val btnscan=findViewById<Button>(R.id.button)
+        btnscan.setOnClickListener {
+            scanQR()
+        }
+    }
+
+    private fun scanQR() {
+        val options = ScanOptions()
+        options.setDesiredBarcodeFormats(ScanOptions.QR_CODE)
+        options.setPrompt("Scan a barcode")
+        options.setCameraId(0) // Use a specific camera of the device
+        options.setBeepEnabled(true)
+        options.setBarcodeImageEnabled(true)
+        options.captureActivity = (CaptureAct::class.java)
+        barcodeLauncher.launch(ScanOptions())
+
+    }
+
+    private val barcodeLauncher: ActivityResultLauncher<ScanOptions> = registerForActivityResult(
+        ScanContract()
+    ) { result ->
+        if (result.contents == null) {
+            Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG)
+                .show()
+        }
+    }
+
+
+}
+
+
+class CaptureAct: CaptureActivity(){
+
+}
+
+
+
